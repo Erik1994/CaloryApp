af0a238c80768db15015048c5d4c1f6f961d685e
Erik Antonyan <erik.antonyan1994@gmail.com>
2023-08-13 14:49:15 +0400

    Add missing dependencies

--- tracker/tracker_data/build.gradle.kts
+++ tracker/tracker_data/build.gradle.kts
@@ -16,4 +16,13 @@ android {
 dependencies {
     "implementation"(project(Modules.core))
     "implementation"(project(Modules.trackerDomain))
+
+    "implementation"(Retrofit.okHttp)
+    "implementation"(Retrofit.retrofit)
+    "implementation"(Retrofit.okHttpLoggingInterceptor)
+    "implementation"(Retrofit.gsonConverter)
+
+    "kapt"(Room.roomCompiler)
+    "implementation"(Room.roomKtx)
+    "implementation"(Room.roomRuntime)
 }
