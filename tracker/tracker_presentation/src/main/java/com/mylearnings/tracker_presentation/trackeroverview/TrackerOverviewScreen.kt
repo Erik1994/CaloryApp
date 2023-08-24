d70999ed6e87183d3cb2e1034b06cdb63fe59106
Erik Antonyan <erik.antonyan1994@gmail.com>
2023-08-24 20:26:05 +0400

    Implement day selector

--- tracker/tracker_presentation/src/main/java/com/mylearnings/tracker_presentation/trackeroverview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/mylearnings/tracker_presentation/trackeroverview/TrackerOverviewScreen.kt
@@ -1,6 +1,9 @@
 package com.mylearnings.tracker_presentation.trackeroverview
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -9,6 +12,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mylearnings.core.util.UiEvent
 import com.mylearnings.core_ui.dimension.LocalSpacing
+import com.mylearnings.tracker_presentation.trackeroverview.components.DaySelector
 import com.mylearnings.tracker_presentation.trackeroverview.components.NutrientsHeader
 
 @Composable
@@ -24,8 +28,21 @@ fun TrackerOverviewScreen(
             .fillMaxSize()
             .padding(bottom = spacing.spaceMedium)
     ) {
-        item { 
+        item {
             NutrientsHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(date = state.date,
+                onPreviousDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                },
+                onNextDayClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
     }
 }
